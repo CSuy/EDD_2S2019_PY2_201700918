192dfb19ed3e927a9fa7877af823206cbda722d3
Miriam Leticia Vasquez Siu <cristiansuy98@gmail.com>
2019-11-12 12:21:38 -0600

    arbol AVL

--- src/Estructuras/ArbolAVL.java
+++ src/Estructuras/ArbolAVL.java
@@ -6,6 +6,10 @@
 package Estructuras;
 
 import Nodos.Nodo_AVL;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  *
@@ -22,19 +26,155 @@ public class ArbolAVL {
         return this.root == null;
     }
     
-    public void Insertar(String nombre_archivo, String contenido, String creacion, String propietario){
-        Nodo_AVL nuevo = new Nodo_AVL(nombre_archivo, contenido, creacion, propietario);
-        Insertar_AVL(nuevo,root);
+    private int Altura(Nodo_AVL raiz){
+        if(raiz == null){
+            return 0;
+        }
+        return raiz.getAltura();
     }
     
-    private void Insertar_AVL(Nodo_AVL nuevo, Nodo_AVL raiz){
+    private int Factor_Equilibrio(Nodo_AVL raiz){
+        if(raiz == null){
+            return 0;
+        }
+        return Altura(raiz.getIzquierdo()) - Altura(raiz.getDerecho());
+    }
+    
+    private Nodo_AVL rotacionI(Nodo_AVL raiz){
+        Nodo_AVL y = raiz.getDerecho();
+        Nodo_AVL x = y.getIzquierdo();
+        y.setIzquierdo(raiz);
+        raiz.setDerecho(x);
+        int altura1 = 1 + Math.max(Altura(raiz.getIzquierdo()), Altura(raiz.getDerecho()));
+        int altura2 = 1 + Math.max(Altura(y.getIzquierdo()), Altura(y.getDerecho()));
+        raiz.setAltura(altura1);
+        y.setAltura(altura2);
+        int balanceo1 = Factor_Equilibrio(raiz);
+        raiz.setFactor_Equilibrio(balanceo1);
+        int balanceo2 = Factor_Equilibrio(y);
+        y.setFactor_Equilibrio(balanceo2);
+        return y;
+    }
+    
+    private Nodo_AVL rotacionD(Nodo_AVL raiz){
+        Nodo_AVL y = raiz.getIzquierdo();
+        Nodo_AVL x = y.getDerecho();
+        y.setDerecho(raiz);
+        raiz.setIzquierdo(x);
+        int altura1 = 1 + Math.max(Altura(raiz.getIzquierdo()), Altura(raiz.getDerecho()));
+        int altura2 = 1 + Math.max(Altura(y.getIzquierdo()), Altura(y.getDerecho()));
+        raiz.setAltura(altura1);
+        y.setAltura(altura2);
+        int balanceo1 = Factor_Equilibrio(raiz);
+        raiz.setFactor_Equilibrio(balanceo1);
+        int balanceo2 = Factor_Equilibrio(y);
+        y.setFactor_Equilibrio(balanceo2);
+        return y;
+    }
+    
+    public void Insertar(String nombre_archivo, String contenido, String creacion, String propietario){
+        Nodo_AVL nuevo = new Nodo_AVL(nombre_archivo, contenido, creacion, propietario);
+        this.root = Insertar_AVL(nuevo,root);
+    }
+    
+    private Nodo_AVL Insertar_AVL(Nodo_AVL nuevo, Nodo_AVL raiz){
         if(vacia() == true){
-            raiz = nuevo;
-            
+            return nuevo;
+        }else if(nuevo.getContenido().compareTo(raiz.getContenido()) == -1){
+            raiz.setIzquierdo(Insertar_AVL(nuevo,raiz.getIzquierdo()));
+        }else{
+            raiz.setDerecho(Insertar_AVL(nuevo,raiz.getDerecho()));
+        }
+        int balanceo = Factor_Equilibrio(raiz);
+        raiz.setFactor_Equilibrio(balanceo);
+        //Casos de Rotacion para AVL
+        //Caso 1:
+        if(balanceo > 1 && nuevo.getContenido().compareTo(raiz.getIzquierdo().getContenido()) == -1){
+            return rotacionD(raiz);
+        }
+        if(balanceo < -1 && nuevo.getContenido().compareTo(raiz.getIzquierdo().getContenido()) == 1){
+            return rotacionI(raiz);
+        }
+        if(balanceo > 1 && nuevo.getContenido().compareTo(raiz.getIzquierdo().getContenido()) == 1){
+            raiz.setIzquierdo(rotacionI(raiz.getIzquierdo()));
+            return rotacionD(raiz);
+        }
+        if(balanceo < -1 && nuevo.getContenido().compareTo(raiz.getDerecho().getContenido()) == -1){
+            raiz.setDerecho(rotacionD(raiz.getDerecho()));
+            return rotacionI(raiz);
+        }
+        return raiz;
+    }
+    
+    public void Graficar(String user){
+        Nodo_AVL aux = root;
+        String archivo = "ArbolAVL_" + user + ".jpg";
+        try{
+            String ruta = "Reportes/Arbol_AVL.dot";
+            String contenido = "digraph guia{ \n";
+            contenido +="node [shape = record];\\n";
+            contenido += graficar1(aux);
+            contenido += "\n}";
+            File file = new File(ruta);
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(contenido);
+            bw.close();
+            try{
+                String cmd = "dot -Tjpg " + ruta + " -o " + "Reportes/" + archivo;
+                try {
+                    Runtime.getRuntime().exec(cmd);
+                } catch (IOException ex) {
+                    System.out.println("No se genero a imagen");
+                }
+            }catch(Exception e){
+                System.out.println("No se genero a imagen");
+            }
+        }catch(IOException ex){
+            System.out.println("Se produjo un error con el archivo .dot");
         }
     }
     
-    
+    private String graficar1(Nodo_AVL raiz){
+        String cuerpo="";
+        if(raiz != null){
+            cuerpo += "\"";
+            cuerpo += "Nombre Archivo: " + raiz.getNombre_Archivo();
+            cuerpo += "\n Contenido: " + raiz.getContenido();
+            cuerpo +=  "\n FE: " + raiz.getFactor_Equilibrio();
+            cuerpo += "\n Altura: " + raiz.getAltura();
+            cuerpo += "\n Creacion: " + raiz.getCreacion();
+            cuerpo += "\n Propietario: " + raiz.getUsuario();
+            cuerpo += "\"";
+            cuerpo += ";";
+            if(raiz.getIzquierdo() != null){
+                cuerpo += "\"";
+                cuerpo += "Nombre Archivo: " + raiz.getNombre_Archivo();
+                cuerpo += "\n Contenido: " + raiz.getContenido();
+                cuerpo +=  "\n FE: " + raiz.getFactor_Equilibrio();
+                cuerpo += "\n Altura: " + raiz.getAltura();
+                cuerpo += "\n Creacion: " + raiz.getCreacion();
+                cuerpo += "\n Propietario: " + raiz.getUsuario();
+                cuerpo += "\"";
+                cuerpo += "-> \n";
+                cuerpo += graficar1(raiz.getIzquierdo());
+            }
+            if(raiz.getDerecho() != null){
+                cuerpo += "\"";
+                cuerpo += "Nombre Archivo: " + raiz.getNombre_Archivo();
+                cuerpo += "\n Contenido: " + raiz.getContenido();
+                cuerpo +=  "\n FE: " + raiz.getFactor_Equilibrio();
+                cuerpo += "\n Altura: " + raiz.getAltura();
+                cuerpo += "\n Creacion: " + raiz.getCreacion();
+                cuerpo += "\n Propietario: " + raiz.getUsuario();
+                cuerpo += "\"";
+                cuerpo += "-> \n";
+                cuerpo += graficar1(raiz.getDerecho());
+            }
+        }
+        return cuerpo;
+    }
+ 
 
     public Nodo_AVL getRoot() {
         return root;
--- src/edd_proyecto2/EDD_Proyecto2.java
+++ src/edd_proyecto2/EDD_Proyecto2.java
@@ -1,6 +1,7 @@
 package edd_proyecto2;
 
 import Aplicacion.Inicio_Sesion;
+import Estructuras.ArbolAVL;
 import Estructuras.TablaHash;
 import Nodos.Nodo_Hash;
 import java.security.NoSuchAlgorithmException;
@@ -12,20 +13,37 @@ import java.util.Scanner;
 public class EDD_Proyecto2 {
     
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        /*System.out.println("Prueba de base64");
-        String cadena = "ESTRUCTURA DE DATOS SECCION A 0919";
-        String encoder = Base64.getEncoder().encodeToString(cadena.getBytes());
-        System.out.println("Este es el resultado del encoder: " + encoder);
-        byte[] encode_base64 = Base64.getDecoder().decode(encoder);
-        String cadena1 = new String(encode_base64);
-        System.out.println("Esto es el contenido de encoder: " + cadena1);
-        String cadena2 = "\"{\"INDEX\": 2, \"TIMESTAMP\": \"02-10-19-::14:30:25\", \"CLASS\": \"Estructuras de datos\", \"DATA\": \"{\"value\":\"201403525-Nery\",\"left\":{\"value\":\"201212963-Andres\",\"left\":{\"value\":\"201005874-Estudiante1\",\"left\":null,\"right\":null},\"right\":{\"value\":\"201313526-Alan\",\"left\":null,\"right\":null}},\"right\":{\"value\":\"201403819-Anne\",\"left\":{\"value\":\"201403624-Fernando\",\"left\":null,\"right\":null},\"right\":{\"value\":\"201602255-Estudiante2\",\"left\":null,\"right\":null}}}\", \"PREVIOUSHASH\": \"fd5f6d5fdfdf232Y232312QW12196255\", \"HASH\": \"55904c5f612680c8a13b86dde7e64b96d5643afb65ae2a27d54b76c9f03a93ec\"}" ;
-        SHA256 sha = new SHA256();
-        String prueba = sha.toHexString(sha.getSHA(cadena2));
-        System.out.println("El Hash es: " + prueba);
-        System.out.println(cadena2);*/
-        Inicio_Sesion i = new Inicio_Sesion();
-        i.setVisible(true);
+        //Inicio_Sesion i = new Inicio_Sesion();
+        //i.setVisible(true);
+        ArbolAVL a = new ArbolAVL();
+        Scanner n = new Scanner(System.in);
+        Scanner n1 = new Scanner(System.in);
+        Scanner n2 = new Scanner(System.in);
+        boolean menu = true;
+        int contador = 1;
+        String nombre = "archivo1";
+        while (menu == true) {
+            System.out.println("Elija 1, 2 o 3");
+            int j = n.nextInt();
+            switch (j) {
+                case 1:
+                    System.out.println("Se ingreso un archivo al arbol");
+                    a.Insertar(nombre+contador, "xD", "hoy", "user1");
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                default:
+                    menu = false;
+                    break;
+            }
+        }
         /*TablaHash t = new TablaHash();
         Scanner n = new Scanner(System.in);
         Scanner n1 = new Scanner(System.in);
